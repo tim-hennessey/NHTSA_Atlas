46d06326ac6b5239610239c615805141d2e76ca5
tim.hennessey <Tim.Hennessey@deutschinc.com>
2017-10-12 11:27:54 -0700

    Resizes

---
+++ app/728x90/scripts/Animation.js
@@ -0,0 +1,69 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+    var tl1 = new TimelineMax(),
+        atlas,
+        txt1,
+        rating,
+        cta_txt,
+        cta_arrow,
+        curtain,
+        buttonExit,
+        buttonLegal,
+        legal;
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+        rating = document.getElementById('rating');
+        txt1 = document.getElementById('txt1');
+        atlas = document.getElementById('atlas');
+        cta_txt = document.getElementById('cta_txt');
+        cta_arrow = document.getElementById('cta_arrow');
+        curtain = document.getElementById('curtain');
+        buttonExit = document.getElementById('button-exit');
+        buttonLegal = document.getElementById('button-legal');
+        legal = document.getElementById('legal');
+
+        buttonExit.addEventListener('mouseover', function () {
+            TweenMax.to(cta_arrow, .25, {x: 5});
+            TweenMax.to(cta_arrow, .25, {x: 0, delay: .25});
+        });
+
+        buttonLegal.addEventListener('mouseover', function () {
+            TweenMax.to(legal, .25, {opacity: 1});
+        });
+
+        buttonLegal.addEventListener('mouseout', function () {
+            TweenMax.to(legal, .25, {opacity: 0});
+        });
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+        tl1.to(curtain, .5, {opacity: 0})
+            .from(atlas, .5, {x:"+=20", y:"-=5", scale:.9, ease: Sine.easeOut}, "-=.5")
+            .to(txt1, .5, {opacity: 1})
+            .to(rating, .5, {opacity: 1})
+            .to(cta_txt, .5, {opacity: 1})
+            .to(cta_arrow, .5, {opacity: 1}, "-=.5");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
